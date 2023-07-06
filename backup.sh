f8986801cb060d3ae650db7dd893b3fc1ec55a35
metozz <79897762+m3tozz@users.noreply.github.com>
2023-07-06 23:54:42 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 # https://github.com/m3tozz/NeoCat.git
 	clear
 	echo -e "\033[0;31mBacking Up...\033[1;36m"
-	mkdir -p Backup
-	cp -r /home/$USER/.config/neofetch Backup
+	mkdir -p Backup-$(date +%Y-%m-%d-%H:%M:%S)	
+	cp -r /home/$USER/.config/neofetch Backup-$(date +%Y-%m-%d-%H:%M:%S)
 	clear
 	echo -e "\033[31m Backed Up!\033[0m"
