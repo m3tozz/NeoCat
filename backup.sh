7ce7b0987efad624fa46cb404d9cf7400d8d1211
metozz <79897762+m3tozz@users.noreply.github.com>
2023-07-06 09:31:02 +0300

    Backup the neofetch configuration!

---
+++ backup.sh
@@ -0,0 +1,8 @@
+# Made By M3TOZZ
+# https://github.com/m3tozz/NeoCat.git
+	clear
+	echo -e "\033[0;31mBacking Up...\033[1;36m"
+	mkdir -p Backup
+	cp -r /home/$USER/.config/neofetch Backup
+		clear
+	echo -e "\033[31m Backed Up!\033[0m"
