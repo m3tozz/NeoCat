2f5746f2612ab535a894bdefa1689633c898e39c
metozz <79897762+m3tozz@users.noreply.github.com>
2024-11-27 20:58:44 +0300

    Update neocat.sh

--- neocat/script/neocat.sh
+++ neocat/script/neocat.sh
@@ -68,7 +68,7 @@ echo -e '\033[1;36m
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                       
 '
 echo -e '
-\e[1;34m[01]\e[0;32mSmall Themes \e[1;35m[02]\e[0;32mLarge Themes\e[1;34m \e[1;31m[A]\e[0;32mAbout \e[1;31m[B]\e[0;32mBackup \e[1;31m[x]\e[0;32mExit
+\e[1;34m[01]\e[0;32mSmall Themes \e[1;35m[02]\e[0;32mLarge Themes\e[1;34m \033[0;33m[C]\e[0;32mCommunity-Themes \e[1;31m[A]\e[0;32mAbout \e[1;31m[B]\e[0;32mBackup \e[1;31m[x]\e[0;32mExit
 '
         echo -ne "\e[1;31mm3tozz\033[0;36m@\033[1;33mneocat\n\e \033[0;36m↳\033[0m " ; read islem
 }
@@ -86,6 +86,12 @@ elif [[ $islem == 2 || $islem == 02 ]]; then
 	clear
 	cd Large-Themes/
 	bash start.sh
+ elif [[ $islem == c || $islem == C ]]; then
+    clear
+    echo -e '\033[0;33mTo add your own neofetch configuration to the NeoCat community,'
+    echo -e '\033[0;31mAdd your own configuration at https://github.com/m3tozz/neocat-community-themes'
+    echo -e 'and submit a Pull Request..\e[0m'
+    echo -e "\e[3m\e[1;34mThis folder is currently empty\e[0m"
  elif [[ $islem == a || $islem == A ]]; then
 	clear
 echo -e '
