48bbb8b9b3afff1951b1a7f86f0e9fb1ddb31668
metozz <79897762+m3tozz@users.noreply.github.com>
2024-12-17 16:03:36 +0300

    Update neocat.sh

--- neocat.sh
+++ neocat.sh
@@ -134,16 +134,15 @@ elif [[ $islem == 2 || $islem == 02 ]]; then
 	bash start.sh
  elif [[ $islem == c || $islem == C ]]; then
 	clear
-	cd  /home/$USER/
 	git clone https://github.com/m3tozz/neocat-community-themes.git
  	clear
  	cd neocat-community-themes
  	echo -e '\033[0;33mTo add your own neofetch configuration to the NeoCat community,'
 	echo -e '\033[0;31mAdd your own configuration at https://github.com/m3tozz/neocat-community-themes'
 	echo -e 'and submit a Pull Request..\e[0m'
+ 	echo -e "\033[1;32m Themes Uploaded by Our Community"
  	ls
- 	echo -e '\033[0;33mYou need to manually install the themes in this folder into .config/neofetch.'
-  	cd
+ 	echo -e '\033[0;33mYou need to manually install the themes in this folder into .config/neofetch.\e[0m'
 elif [[ $islem == x || $islem == X ]]; then
 	clear
 
