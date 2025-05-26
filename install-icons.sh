5b2db11191618cc8b6eccca43a9940649df50f69
Metin Zühre <79897762+m3tozz@users.noreply.github.com>
2025-05-27 00:39:46 +0300

    Update install-icons.sh

--- install-icons.sh
+++ install-icons.sh
@@ -1,37 +1,39 @@
-# Made By M3TOZZ
-# https://github.com/m3tozz/NeoCat.git
-clear
+#!/bin/bash
 
-banner(){
 echo -e '\033[1;36m
  _   _             ____      _   
 | \ | | ___  ___  / ___|__ _| |_ 
-|  \| |/ _ \/ _ \| |   / _` | __|Install-Terminal-Icons
+|  \| |/ _ \/ _ \| |   / _` | __| Install-Terminal-Icons
 | |\  |  __/ (_) | |__| (_| | |_ 
-|_| \_|\___|\___/ \____\__,_|\__|
-                                     
-'
+|_| \_|\___|\___/ \____\__,_|\__|                         
+\033[0m'
 
-        echo -ne "\e[0;32mInstall Terminal Icons (icons-in-terminal) (Y/N)" ; read islem
-}
+read -p "Do you want to install CascadiaCode Nerd Font? (Y/N): " choice
 
-banner
-if [[ $islem == y || $islem == Y ]]; then
-	clear
-	echo -e "\033[0;31mSetting Up...\033[1;36m"
-	git clone https://github.com/sebastiencs/icons-in-terminal.git
-	cd icons-in-terminal
-	./install.sh
-        clear
-	echo -e "\033[1;31mNeoCat - NeoFetch Theme Pack!\033[0m"
-       	sleep 3
-	./print_icons.sh
-	echo -e "\033[1;31mInstalled! \033[1;36mNow Start A New Terminal :) \033[0m"
+if [[ "$choice" =~ ^[Yy]$ ]]; then
+    FONT_DIR="$HOME/.fonts"
 
-elif [[ $islem == n || $islem == N ]]; then
-	clear
-echo -e "\033[0m GoodBye."
-	exit 1
- else
-	echo -e '\033[36;40;1m Wrong transaction number!\033[0m'	
+    if [ ! -d "$FONT_DIR" ]; then
+        echo -e "\033[1;32mCreating ~/.fonts directory...\033[0m"
+        mkdir -p "$FONT_DIR"
+    else
+        echo -e "\033[1;33m~/.fonts directory already exists.\033[0m"
+    fi
+
+    echo -e "\033[1;34mDownloading CascadiaCode Nerd Font...\033[0m"
+    wget -q -P "$FONT_DIR" https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/CascadiaCode.zip
+
+    echo -e "\033[1;34mExtracting font files...\033[0m"
+    unzip -oq "$FONT_DIR/CascadiaCode.zip" -d "$FONT_DIR"
+
+    echo -e "\033[1;34mCleaning up...\033[0m"
+    rm "$FONT_DIR/CascadiaCode.zip"
+
+    echo -e "\033[1;34mRefreshing font cache...\033[0m"
+    fc-cache -fv
+
+    echo -e "\n\033[1;32mInstallation complete."
+    echo -e "Please select 'CascadiaCode Nerd Font' in your terminal settings.\033[0m"
+else
+    echo -e "\033[1;31mInstallation canceled by user.\033[0m"
 fi
