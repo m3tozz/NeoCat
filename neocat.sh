fe4f7997820a3cce86c5450f6f931cac950c0f08
Metin Zühre <79897762+m3tozz@users.noreply.github.com>
2025-07-05 17:59:03 +0300

    Update neocat.sh

--- neocat.sh
+++ neocat.sh
@@ -168,7 +168,7 @@ echo -e '
     echo -e "$blue##########################################################$tp"
     echo -e "    Create by           ":" $red M3TOZZ$tp"
     echo -e "    Github              ":" $red https://github.com/m3tozz$tp"
-    echo -e "    Instagram           ":" $red @textzuhree$tp"
+    echo -e "    Community Server    ":" $red discord.com/invite/sQwYCZer95$tp"
     echo -e "    Version             ":" $red ${version} $tp"
     echo -e "$blue##########################################################$tp"
     exit 1
